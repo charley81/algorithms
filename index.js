d57c6a8f1e2f70887e559c1514d1ef07fec6cda5
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-02 13:09:18 -0500

    added anagram algorigth

--- index.js
+++ index.js
@@ -218,6 +218,29 @@ function same2(arr1, arr2) {
   }
   return true
 }
-console.log(same2([1, 2, 3], [4, 1, 9]))
-console.log(same2([1, 4, 3], [9, 16, 1]))
-console.log(same2([1, 2, 3, 1], [1, 4, 9]))
+// console.log(same2([1, 2, 3], [4, 1, 9]))
+// console.log(same2([1, 4, 3], [9, 16, 1]))
+// console.log(same2([1, 2, 3, 1], [1, 4, 9]))
+
+// Frequency Counter - validAnagram
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false
+  const check = {}
+  for (let i = 0; i < str1.length; i++) {
+    const letter = str1[i]
+    check[letter] ? (check[letter] += 1) : (check[letter] = 1)
+  }
+  for (let i = 0; i < str2.length; i++) {
+    const letter = str2[i]
+    if (!check[letter]) {
+      return false
+    } else {
+      check[letter] -= 1
+    }
+  }
+  return true
+}
+
+console.log(validAnagram('axx', 'xax'))
+console.log(validAnagram('car', 'rat'))
