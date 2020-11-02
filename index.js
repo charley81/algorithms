783c92bca642a083429e43691cdada47fb559535
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-02 11:55:45 -0500

    added frequency counter algorithm

--- index.js
+++ index.js
@@ -160,6 +160,64 @@ function factors(n) {
   return result
 }
 
-console.log(factors(15)) // [1,3,5,15]
-console.log(factors(16)) // [1,2,4,8,16]
-console.log(factors(17)) // [1,17]
+// console.log(factors(15)) // [1,3,5,15]
+// console.log(factors(16)) // [1,2,4,8,16]
+// console.log(factors(17)) // [1,17]
+
+// ========== SORTING ==========
+
+// Bubble Sort
+const bubbleSort = nums => {
+  do {
+    let swapped = false
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i]
+        nums[i] = nums[i + 1]
+        nums[i + 1] = temp
+        swapped = true
+      }
+    }
+  } while (swapped)
+}
+
+// FREQUENCY COUNTERS
+
+// write a function called same, which accepts two arrays. The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same.
+
+// O(n^2) nested loops
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
+
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2)
+    if (correctIndex === -1) return false
+    arr2.splice(correctIndex, 1)
+  }
+  return true
+}
+
+// console.log(same([1, 2, 3], [4, 1, 9]))
+// console.log(same([1, 2, 3], [4, 1]))
+// console.log(same([1, 2, 3, 1], [1, 4, 9]))
+
+// O(n)
+function same2(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false
+  let freqCounter1 = {}
+  let freqCounter2 = {}
+  for (let val of arr1) {
+    freqCounter1[val] = (freqCounter1[val] || 0) + 1
+  }
+  for (let val of arr2) {
+    freqCounter2[val] = (freqCounter2[val] || 0) + 1
+  }
+  for (let key in freqCounter1) {
+    if (!(key ** 2 in freqCounter2)) return false
+    if (freqCounter2[key ** 2] !== freqCounter1[key]) return false
+  }
+  return true
+}
+console.log(same2([1, 2, 3], [4, 1, 9]))
+console.log(same2([1, 4, 3], [9, 16, 1]))
+console.log(same2([1, 2, 3, 1], [1, 4, 9]))
