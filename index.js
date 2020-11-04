dd3c38e4c4530511b5d030ff9e9c6d9f705ae2bb
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-04 13:22:35 -0500

    added subSequence algo

--- index.js
+++ index.js
@@ -382,3 +382,33 @@ console.log(checkDuplicates('abcd'))
 console.log(checkDuplicates('hola'))
 console.log(checkDuplicates('aacbd'))
 console.log(checkDuplicates('add'))
+
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+function averagePair(arr, avg) {
+  let start = 0
+  let end = arr.length - 1
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2
+    if (avg === num) return true
+    else if (avg < num) start++
+    else end--
+  }
+  return false
+}
+
+console.log(averagePair([1, 3, 5, 7, 9], 8))
+
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string from a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing
+function isSubsequence(str1, str2) {
+  let i = 0
+  let j = 0
+  if (!str1) return false
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++
+    if (i === str1.length) return true
+    j++
+  }
+  return false
+}
+
+console.log(isSubsequence('hello', 'worldhello'))
